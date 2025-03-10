af6653fdaa161a1e0dd0e38e49dd15011bf371c4
Om Mathur <133551651+ommathur@users.noreply.github.com>
2025-03-10 16:24:27 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this, I have made the changes to the hello.cpp file \n";
+  cout << "I have successfully built and run this, I have made the changes to the file. \n";
   return 0;
 }
